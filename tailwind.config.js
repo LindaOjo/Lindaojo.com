b87fa436d9f5d5a040d9cd02a32eaeb92798eeae
LindaOjo <linda@buycoins.africa>
2020-07-11 00:06:33 +0100

    fix attempt 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,6 @@
 module.exports = {
   purge: {
-    content: [
-        'components/**/*.vue',
-        'layouts/**/*.vue',
-        'pages/**/*.vue',
-        'plugins/**/*.js',
-        'nuxt.config.js'
-      ]
+ 
   },
   theme: {
     extend: {},
