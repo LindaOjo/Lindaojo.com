410204698499e004923438bc35182a8661181069
LindaOjo <linda@buycoins.africa>
2020-09-12 12:24:27 +0100

    add duration to latest

--- src/components/Latest.vue
+++ src/components/Latest.vue
@@ -4,10 +4,16 @@
           :key="post.node.id">
         <g-link class="post-card hover:-translate-y-1 hover:scale-105 transition delay-100 duration-300 ease-in-out transform"
                 :to="post.node.path">
-            <div class="block">
-                <h3 class="h3 my-2">{{post.node.title}}</h3>
-                <p class="excerpt">{{post.node.excerpt}} </p>
+            <div class="inline-block w-full">
+                <div>
+                    <h3 class="h3 my-2">{{post.node.title}}</h3>
+                    <p class="excerpt">{{post.node.excerpt}} </p>
+                </div>
+                <div class="text-right">
+                    <p>{{post.node.duration}}</p>
+                </div>
             </div>
+            
         </g-link>
     </span>
   </section>
@@ -21,6 +27,7 @@
                 isBlogPost
                 path
                 title
+                duration
                 excerpt
                 date
             }
